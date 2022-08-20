d144328df64ca1e073bc3bb1a5ede566c7b2711e
luke3 <luke3@ualberta.ca>
2022-08-19 19:40:20 -0600

    Add info bar

--- static/script.js
+++ static/script.js
@@ -1,9 +1,18 @@
 
+let die = ["A", "B", "C", "D",
+           "E", "F", "G", "H",
+           "I", "J", "K", "L",
+           "M", "N", "O", "P"];
 
 let dieCoords = ["00", "01", "02", "03",
                  "10", "11", "12", "13",
                  "20", "21", "22", "23",
                  "30", "31", "32", "33"];
+
+let selected = [false, false, false, false,
+                false, false, false, false,
+                false, false, false, false,
+                false, false, false, false];
 
 let word = "";
 
@@ -14,15 +23,33 @@ let word = "";
         for (let i = 0; i < dieCoords.length; i++) {
             document.getElementById(dieCoords[i]).addEventListener(
                 "click", function (event) {
-                    document.getElementById(dieCoords[i]).classList.add("selectedDie");
+                    if (!selected[i]) {
+                        document.getElementById(dieCoords[i]).classList.add("selectedDie");
+                        word += die[i];
+                        document.getElementById("word").innerHTML = word;
+                        selected[i] = true;
+                    }
                 }
             );
         }
+
+        document.getElementById("reset").addEventListener("click",
+            function (event) {
+                resetWord();
+                word = "";
+                document.getElementById("word").innerHTML = word;
+            }
+        );
     }
 
     function resetWord() {
         for (let i = 0; i < dieCoords.length; i++) {
             document.getElementById(dieCoords[i]).classList.remove("selectedDie");
         }
+
+        selected = [false, false, false, false,
+                    false, false, false, false,
+                    false, false, false, false,
+                    false, false, false, false];
     }
 })(window, document, undefined);
